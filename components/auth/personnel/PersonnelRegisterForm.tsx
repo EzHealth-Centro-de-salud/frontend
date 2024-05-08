73902689266c6f9c1712a38404da0947459c0a39
DanielSanAI <daniel.barrios@alumnos.ucn.cl>
2024-05-08 02:19:45 -0400

    feat: Update personnel login page and form components

--- components/auth/personnel/PersonnelRegisterForm.tsx
+++ components/auth/personnel/PersonnelRegisterForm.tsx
@@ -12,41 +12,62 @@ import { Label } from "@/components/ui/label";
 import { REGISTER_PERSONNEL_MUTATION } from "../../apollo/mutations";
 import client from "../../apollo/ApolloClient";
 
+interface PersonnelFormState {
+  rut: string;
+  password: string;
+  firstName: string;
+  middleName: string;
+  surname: string;
+  secondSurname: string;
+  email: string;
+  role: string;
+  specialty: string;
+  idBranch: string;
+}
+
 function PersonnelRegisterForm() {
-  const [rut, setRut] = useState("");
-  const [password, setPassword] = useState("");
-  const [first_name, setFirst_name] = useState("");
-  const [middle_name, setMiddle_name] = useState("");
-  const [surname, setSurname] = useState("");
-  const [second_surname, setSecond_surname] = useState("");
-  const [email, setEmail] = useState("");
-  const [role, setRole] = useState("");
-  const [specialty, setSpecialty] = useState("");
-  const [branchId, setBranchId] = useState("");
+  const [personnelFormState, setPersonnelFormState] =
+    useState<PersonnelFormState>({
+      rut: "",
+      password: "",
+      firstName: "",
+      middleName: "",
+      surname: "",
+      secondSurname: "",
+      email: "",
+      role: "",
+      specialty: "",
+      idBranch: "",
+    });
+
   const [registerPersonnel] = useMutation(REGISTER_PERSONNEL_MUTATION, {
     client,
   });
+
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setPersonnelFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault(); //evita que se refresque la pagina al enviar el formulario
 
     const { data, errors } = await registerPersonnel({
       variables: {
-        CreatePersonnelInput: {
-          rut,
-          password,
-          first_name,
-          middle_name,
-          surname,
-          second_surname,
-          email,
-          role,
-          specialty,
-          branchId,
-        },
+        CreatePersonnelInput: { ...personnelFormState },
       },
     });
+
     console.log(data);
-    if (data?.createPatient.success) {
+    if (data?.createPersonnel.success) {
       window.alert("Usuario creado");
       history.back();
     } else {
@@ -64,39 +85,43 @@ function PersonnelRegisterForm() {
           <Input
             className="bg-[#26313c] text-white"
             required
-            value={rut}
-            onChange={(e) => setRut(e.target.value)}
+            value={personnelFormState.rut}
+            onChange={handleInputChange}
             id="rut"
             type="text"
             maxLength={12}
+            name="rut"
           />
+          {errors.rut && <span className="text-red-500">{errors.rut}</span>}
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className="grid w-full items-center gap-1.5">
-            <Label className="text-white" htmlFor="first_name">
+            <Label className="text-white" htmlFor="firstName">
               Primer Nombre
             </Label>
             <Input
               className="bg-[#26313c] text-white"
               required
-              value={first_name}
-              onChange={(e) => setFirst_name(e.target.value)}
-              id="first_name"
+              value={personnelFormState.firstName}
+              onChange={handleInputChange}
+              id="firstName"
               type="text"
               maxLength={50}
+              name="firstName"
             />
           </div>
           <div className="grid w-full items-center gap-1.5">
-            <Label className="text-white" htmlFor="middle_name">
+            <Label className="text-white" htmlFor="middleName">
               Segundo Nombre
             </Label>
             <Input
               className="bg-[#26313c] text-white"
-              value={middle_name}
-              onChange={(e) => setMiddle_name(e.target.value)}
-              id="middle_name"
+              value={personnelFormState.middleName}
+              onChange={handleInputChange}
+              id="middleName"
               type="text"
               maxLength={50}
+              name="middleName"
             />
           </div>
         </div>
@@ -108,25 +133,27 @@ function PersonnelRegisterForm() {
             <Input
               className="bg-[#26313c] text-white"
               required
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
+              value={personnelFormState.surname}
+              onChange={handleInputChange}
               id="surname"
               type="text"
               maxLength={50}
+              name="surname"
             />
           </div>
           <div className="grid w-full items-center gap-1.5">
-            <Label className="text-white" htmlFor="second_surname">
+            <Label className="text-white" htmlFor="secondSurname">
               Apellido Materno
             </Label>
             <Input
               className="bg-[#26313c] text-white"
               required
-              value={second_surname}
-              onChange={(e) => setSecond_surname(e.target.value)}
-              id="second_surname"
+              value={personnelFormState.secondSurname}
+              onChange={handleInputChange}
+              id="secondSurname"
               type="text"
               maxLength={50}
+              name="secondSurname"
             />
           </div>
         </div>
@@ -138,11 +165,12 @@ function PersonnelRegisterForm() {
             <Input
               className="bg-[#26313c] text-white"
               required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={personnelFormState.email}
+              onChange={handleInputChange}
               id="email"
               type="email"
               maxLength={254}
+              name="email"
             />
           </div>
           <div className="grid w-full items-center gap-1.5">
@@ -152,45 +180,62 @@ function PersonnelRegisterForm() {
             <Input
               className="bg-[#26313c] text-white"
               required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={personnelFormState.password}
+              onChange={handleInputChange}
               id="password"
               type="password"
               maxLength={128}
+              name="password"
             />
           </div>
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className="grid w-full items-center gap-1.5">
-            <Label className="text-white" htmlFor="email">
+            <Label className="text-white" htmlFor="specialty">
               Especialidad
             </Label>
             <Input
               className="bg-[#26313c] text-white"
               required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              id="email"
-              type="email"
+              value={personnelFormState.specialty}
+              onChange={handleInputChange}
+              id="specialty"
               maxLength={254}
+              name="specialty"
             />
           </div>
           <div className="grid w-full items-center gap-1.5">
-            <Label className="text-white" htmlFor="sex">
+            <Label className="text-white" htmlFor="idBranch">
               Sucursal
             </Label>
             <select
               className="bg-[#26313c] text-white"
               required
-              value={branchId}
-              onChange={(e) => setBranchId(e.target.value)}
-              id="sex"
+              value={personnelFormState.idBranch}
+              onChange={handleInputChange}
+              id="idBranch"
+              name="idBranch"
             >
               <option value="">Seleccione...</option>
               <option value={parseInt("0", 10)}>Coquimbo 1</option>
               <option value={parseInt("1", 10)}>Coquimbo 2</option>
               <option value={parseInt("2", 10)}>La Serena 1</option>
             </select>
+          </div>
+          <div className="grid w-full items-center gap-1.5">
+            <Label className="text-white" htmlFor="role">
+              Rol
+            </Label>
+            <Input
+              className="bg-[#26313c] text-white"
+              required
+              value={personnelFormState.role}
+              onChange={handleInputChange}
+              id="role"
+              type="text"
+              maxLength={50}
+              name="role"
+            />
           </div>
         </div>
 
