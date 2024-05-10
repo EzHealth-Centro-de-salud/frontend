7782e1210ecf55f309f6d416a4c3f78228d32656
DanielSanAI <daniel.barrios@alumnos.ucn.cl>
2024-05-09 21:51:16 -0400

    refactor: Update phone input type in registration form

--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -284,7 +284,7 @@ function RegisterForm() {
               value={phone}
               onChange={(e) => setPhone(e.target.value)}
               id="phone"
-              type="number" 
+              type="text"
               maxLength={9}
             />
           </div>
