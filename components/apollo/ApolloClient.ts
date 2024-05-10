1362763a775bbb2084b87cedccdb7a72404984a8
BRAYAN ALEXIS MALDONADO CARRASCO <brayan.maldonado@alumnos.ucn.cl>
2024-05-09 22:10:07 -0400

    FIX: NEXT_PUBLIC_BACKEND_URL

--- components/apollo/ApolloClient.ts
+++ components/apollo/ApolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: process.env.BACKEND_URL,
+  uri: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 const client = new ApolloClient({
